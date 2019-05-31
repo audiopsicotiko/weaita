85ca266962235e49b417685fd1f9e7f9fe2cac2e
audiopsicotiko <audiopsicotiko@gmail.com>
2019-05-31 15:02:48 -0400

    Add files via upload

---
+++ conf.d/proxy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "# PORTAS DE ACESSO NO SQUID" > /etc/squid/squid.conf
+echo "http_port 7070" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "# NOME DO SERVIDOR" >> /etc/squid/squid.conf
+echo "visible_hostname JoeLinux" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "# ACL DE CONEXAO" >> /etc/squid/squid.conf
+echo "acl accept src $ip" >> /etc/squid/squid.conf
+echo "acl ip url_regex -i $ip" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "# CACHE DO SQUID" >> /etc/squid/squid.conf
+echo "cache_mem 200 MB" >> /etc/squid/squid.conf
+echo "maximum_object_size_in_memory 32 KB" >> /etc/squid/squid.conf
+echo "maximum_object_size 1024 MB" >> /etc/squid/squid.conf
+echo "minimum_object_size 0 KB" >> /etc/squid/squid.conf
+echo "cache_swap_low 90" >> /etc/squid/squid.conf
+echo "cache_swap_high 95" >> /etc/squid/squid.conf
+echo "cache_dir ufs /var/spool/squid 100 16 256" >> /etc/squid/squid.conf
+echo "access_log /var/log/squid/access.log squid" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "" >> /etc/squid/squid.conf
+echo "# ACESSOS ACL" >> /etc/squid/squid.conf
+echo "http_access allow all" >> /etc/squid/squid.conf
+echo "http_access allow all" >> /etc/squid/squid.conf
+echo "http_access deny all" >> /etc/squid/squid.conf
+echo "cache deny all" >> /etc/squid/squid.conf
+service squid restart
